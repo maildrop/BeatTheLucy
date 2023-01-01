1eca009be9fca7b64b5bee4c1dba9524815a84dc
TOGURO Mikito <mit@shalab.net>
2023-01-01 16:31:04 +0900

    行分割の処理を追加
    入力の行分割処理を追加して、BeatTheLucy#queueMessage() に投入する処理を追加

--- src/main/java/com/example/BeatTheLucy.java
+++ src/main/java/com/example/BeatTheLucy.java
@@ -1,5 +1,4 @@
 package com.example;
-
 import java.lang.*;
 import java.util.logging.Logger;
 import java.io.IOException;
@@ -7,6 +6,7 @@ import java.nio.channels.Selector;
 import java.nio.channels.SelectionKey;
 import static java.nio.charset.CoderResult.UNDERFLOW;
 import static java.nio.charset.CoderResult.OVERFLOW;
+
 /**
    Lucy に打ち勝て！
    // com.example.BeatTheLucy
@@ -15,23 +15,204 @@ import static java.nio.charset.CoderResult.OVERFLOW;
 public final class BeatTheLucy{
     private static final Logger logger = Logger.getLogger(BeatTheLucy.class.getName());
 
-    /**
-      実行時エントリーポイント
-     */
-    public static final void main( final String args[] ){
-        System.out.println( "hello world" );
-        beatTheLucy(7777);
-        return;
+    private static class QueueEntry{
+        public SelectionKey selectionKey;
+        public String message;
     }
 
+    private void queueMessage( final QueueEntry queueEntry ){
+        synchronized( queueEntry ){
+            logger.info( String.valueOf( queueEntry.selectionKey ) + " " + String.valueOf( queueEntry.message ) );
+        }
+    }
+    
     /**
        Selector の selectedKeys() が各々行う処理
      */
-    private interface Operation{
+    private static interface Operation{
         public void operate( SelectionKey selectionKey );
     }
+
+    /**
+       クライアントからの要求実行
+    */
+    private final class ReaderOp implements Operation{
+        private final java.nio.ByteBuffer read_buffer;
+        private final java.nio.charset.CharsetDecoder decoder;
+        private final java.nio.CharBuffer cb;
+        private final java.nio.ByteBuffer write_buffer;
+
+        private ReaderOp(){
+            this.read_buffer = java.nio.ByteBuffer.allocate( 8 /* 4096 */ ); // 4k page 
+            this.decoder = java.nio.charset.StandardCharsets.UTF_8.newDecoder();
+            this.cb = java.nio.CharBuffer.allocate( 4096 / 2  );     // 4k page
+            this.write_buffer = java.nio.ByteBuffer.allocate( 4096 );// 4k page
+        }
+
+        /**
+           読み取り後の入力の処理
+           ここでは、ByteBufferの入力を String へ変換する
+         */
+        private final void input_processing(final SelectionKey selectionKey, final boolean endOfInput ){
+            synchronized( decoder ){
+                synchronized( cb ){
+                    final var result = decoder.decode( read_buffer , cb , endOfInput );
+                    assert (result == UNDERFLOW || result == OVERFLOW ): "CorderResultの想定していない戻り値" ;
+                    if( result == UNDERFLOW || result == OVERFLOW ){
+                        // 入力側のByteBufferで 未処理の部分をByteBufferに詰め直す
+                        if( 0 < read_buffer.remaining() ){
+                            final byte remain[] = new byte[read_buffer.remaining()];
+                            read_buffer.get(remain);
+                            read_buffer.rewind();
+                            read_buffer.put(remain);
+                        }else{
+                            read_buffer.rewind();
+                        }
+                    }
+                    
+                    // デコーダの出力の処理
+                    if( result == UNDERFLOW ){
+                        { // デコード済みのCharBufferを処理する
+                            final char c[] = new char[cb.position()];
+                            cb.rewind();
+                            cb.get( c );
+
+                            // 改行単位で分割する
+                            {
+                                int begin = 0;
+                                int pos = 0;
+                                for( ; pos < c.length ; ++pos ){
+                                    if( c[pos] == '\n' ){
+                                        final QueueEntry entry = new QueueEntry();
+                                        synchronized( entry ){
+                                            entry.selectionKey = selectionKey;
+                                            entry.message = new String( c, begin , pos-begin );
+                                        }
+                                        queueMessage( entry );
+                                        begin = pos+1;
+                                    }
+                                }
+                                assert begin <= c.length;
+                                if( begin < c.length ){
+                                    cb.rewind();
+                                    cb.put( c , begin , c.length - begin );
+                                }else if( begin == c.length ){
+                                    cb.clear();
+                                }
+                                
+                                if( endOfInput ){ // 入力終端の場合は残りがあれば全部を String へ変換する
+                                    if( begin < pos ){
+                                        final QueueEntry entry = new QueueEntry();
+                                        entry.message = new String( c, begin , pos-begin );
+                                        queueMessage( entry );
+                                    }else if( begin == pos ){
+
+                                    }
+                                }
+                            }
+                        }
+                    }else if( result == OVERFLOW ){
+                        logger.warning( "OVERFLOW" + String.valueOf( cb.remaining() ));
+                    }else{
+                        logger.warning( "what?");
+                    }
+                }
+            }
+        }
+        
+        /**
+           読み取りの実装
+           @return Channel の終端に到達した場合は -1 が返る それ以外は 0以上の値が返る
+         */
+        private final int do_read( final SelectionKey selectionKey )
+            throws IOException{
+            final java.nio.channels.SocketChannel socket = java.nio.channels.SocketChannel.class.cast( selectionKey.channel() );
+            synchronized( read_buffer ){
+                synchronized( socket ){
+                    for(;;){
+                        assert 0 < read_buffer.remaining();
+
+                        final int read_size = socket.read( read_buffer );
+                        if( read_size == 0 ){
+                            return 0;
+                        }else if( read_size == -1 ){
+                            read_buffer.flip();
+                            this.input_processing( selectionKey, true );
+                            logger.info( "disconnect by " + String.valueOf( socket ) );
+                            return -1;
+                        }
+                        assert 0 < read_size ;
+                        read_buffer.flip();
+                        this.input_processing( selectionKey, false );
+                        continue;
+                    }
+                }
+            }
+        }
+
+        /**
+           書き込みの実装
+           @return 特に意味が無い
+         */
+        private final int do_write( final SelectionKey selectionKey )
+            throws IOException{
+            final java.nio.channels.SocketChannel socket = java.nio.channels.SocketChannel.class.cast( selectionKey.channel() );
+            return 0;
+        }
+        
+        public void operate( final SelectionKey selectionKey ){
+            logger.entering( "ReaderOp" , "operate" );
+            try{
+                final int interestOps = selectionKey.interestOps();
+                if( ((interestOps & SelectionKey.OP_READ) != 0 ) && selectionKey.isReadable() ){
+                    // 読み取りに興味があり、実際に読み取りが可能ならば
+                    final int val = this.do_read( selectionKey ); // 読み取り実行
+                    if( val == -1 ){ 
+                        // 読み取りが終端に達しているので、 selectionKey の interestOps で OP_READ フラグを落とす
+                        selectionKey.interestOps( selectionKey.interestOps() & ( ~SelectionKey.OP_READ ) );
+                    }
+                }else if( ((interestOps & SelectionKey.OP_WRITE) != 0 )&& selectionKey.isWritable() ){
+                    // 書き込みに興味があり、実際に書き込みが可能ならば
+                    this.do_write( selectionKey );
+                }
+
+                // selectionKey.interestOps() が OP_READ OP_WRITE のフラグが共に落ちている時にはもう閉じる
+                if( (selectionKey.interestOps() & ( SelectionKey.OP_READ | SelectionKey.OP_WRITE ) ) == 0 ){
+                    final java.nio.channels.SocketChannel socket = java.nio.channels.SocketChannel.class.cast( selectionKey.channel() );
+                    selectionKey.cancel();
+                    socket.close();
+                }
+
+            }catch( final IOException ioe ){
+                logger.warning( String.valueOf( ioe ));
+                try{
+                    final java.nio.channels.SocketChannel socket = java.nio.channels.SocketChannel.class.cast( selectionKey.channel() );
+                    socket.close();
+                }catch( final IOException closeing_exception ){
+                    logger.severe( String.valueOf( closeing_exception ));
+                }
+            }
+            logger.exiting( "ReaderOp" , "operate" );
+            return;
+        }
+    } // end of ReaderOp 
+
+    // クライアントからのconnectionを受け取る
+    private final class Acceptor implements Operation{
+        public void operate( final SelectionKey selectionKey ){
+            try{
+                final java.nio.channels.ServerSocketChannel sc = java.nio.channels.ServerSocketChannel.class.cast( selectionKey.channel() );
+                final java.nio.channels.SocketChannel channel = sc.accept();
+                channel.configureBlocking( false );
+                logger.info( channel.toString() );
+                channel.register( selectionKey.selector() , SelectionKey.OP_READ , new ReaderOp() );
+            }catch( final IOException ioe ){
+                logger.warning( String.valueOf( ioe ));
+            }
+        }
+    } // end of Acceptor
     
-    public static final void beatTheLucy(final int port){
+    public final void beatTheLucy(final int port){
 
         assert 0 < port ;
         assert port < 65536;
@@ -85,89 +266,12 @@ public final class BeatTheLucy{
         }
     }
 
-    // クライアントからの読み取り実行
-    private static final class ReaderOp implements Operation{
-        private java.nio.ByteBuffer buffer;
-        private java.nio.charset.CharsetDecoder decoder;
-        private java.nio.CharBuffer cb;
-        private ReaderOp(){
-            this.buffer = java.nio.ByteBuffer.allocate( 4096 ); // 4k page 
-            this.decoder = java.nio.charset.StandardCharsets.UTF_8.newDecoder();
-            this.cb = java.nio.CharBuffer.allocate( 4096 ); // 4k page
-        }
-
-        public void operate( final SelectionKey selectionKey ){
-            logger.entering( "ReaderOp" , "operate" );
-            try{
-                assert selectionKey.isReadable() ;
-                final java.nio.channels.SocketChannel socket = java.nio.channels.SocketChannel.class.cast( selectionKey.channel() );
-
-                for(;;){
-                    assert 0 < buffer.remaining();
-                    final int read_size = socket.read( buffer );
-                    if( read_size == 0 ){
-                        break;
-                    }else if( read_size == -1 ){
-                        buffer.flip();
-                        logger.info( "disconnect by " + String.valueOf( socket ) );
-                        selectionKey.cancel();
-                        socket.close();
-                        break;
-                    }else{
-                        assert 0 < read_size ;
-                        buffer.flip();
-                        final var result = decoder.decode( buffer , cb , false );
-                        if( result == UNDERFLOW ){
-                            { // デコード済みのCharBufferを処理する
-                                final int pos = cb.position();
-                                if( cb.hasArray() ){
-                                    
-                                }else{
-                                    
-                                }
-                                cb.rewind();
-                                char c[] = new char[pos];
-                                cb.get( c );
-                                logger.info( "underflow" + " " + new String( c ) );
-                            }
-                            
-                            if( 0 < buffer.remaining() ){
-                                final byte remain[] = new byte[buffer.remaining()];
-                                buffer.get(remain);
-                                buffer.rewind();
-                                buffer.put(remain);
-                            }else{
-                                buffer.rewind();
-                            }
-                            continue;
-                        }else if( result == OVERFLOW ){
-                            logger.warning( "OVERFLOW" + String.valueOf( cb.remaining() ));
-                            
-                        }else{
-                            logger.warning( "what?");
-                        }
-                    }
-                }
-                
-            }catch( final IOException ioe ){
-                logger.warning( String.valueOf( ioe ));
-            }
-            logger.exiting( "ReaderOp" , "operate" );
-        }
-    }
-
-    // クライアントからのconnectionを受け取る
-    private static final class Acceptor implements Operation{
-        public void operate( final SelectionKey selectionKey ){
-            try{
-                final java.nio.channels.ServerSocketChannel sc = java.nio.channels.ServerSocketChannel.class.cast( selectionKey.channel() );
-                final java.nio.channels.SocketChannel channel = sc.accept();
-                channel.configureBlocking( false );
-                logger.info( channel.toString() );
-                channel.register( selectionKey.selector() , SelectionKey.OP_READ , new ReaderOp() );
-            }catch( final IOException ioe ){
-                logger.warning( String.valueOf( ioe ));
-            }
-        }
+    /**
+      実行時エントリーポイント
+     */
+    public static final void main( final String args[] ){
+        System.out.println( "hello world" );
+        new BeatTheLucy().beatTheLucy(7777);
+        return;
     }
 }
