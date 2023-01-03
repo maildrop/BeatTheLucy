ca7ad282550a45ee722cf3a3917a29ce1a5983bd
TOGURO Mikito <mit@shalab.net>
2023-01-04 01:40:41 +0900

    システムプロパティ os.name によるプラットフォームの弁別

--- src/test/java/com/example/Win32.java
+++ src/test/java/com/example/Win32.java
@@ -1,6 +1,7 @@
 package com.example;
 import java.lang.*;
 import java.io.IOException;
+import java.util.Objects;
 
 public final class Win32{
     private static final java.util.logging.Logger logger =
@@ -8,24 +9,31 @@ public final class Win32{
     public static final void main(final String args[] ){
         logger.info( "hello world" );
 
-        final java.nio.file.Path pipePath = java.nio.file.FileSystems.getDefault().getPath("\\\\.\\pipe\\beatTheLucy");
-        logger.info( String.valueOf( pipePath ) );
-        try(
-            final java.nio.channels.FileChannel fileChannel =
-            java.nio.channels.FileChannel.open( pipePath ,
-                                                java.nio.file.StandardOpenOption.CREATE_NEW,
-                                                java.nio.file.StandardOpenOption.READ );
-            ){
-            
-        }catch( final IOException ioe ){
-            logger.severe( String.valueOf( ioe ));
-            /*
-              1月 03, 2023 4:22:33 午前 com.example.Win32 main
-              重大: java.nio.file.NoSuchFileException: \\.\pipe\beatTheLucy
+        if( Objects.toString( System.getProperty("os.name" ))
+            .toLowerCase()
+            .startsWith("windows") ){
 
-              WinAPI の NamedPipe は CreateNamedPipe() で作る必要があるので、FileChannel では作成出来ない。
-             */
+            logger.info( String.format( "${os.name} = %s" , System.getProperty("os.name")  ));
+            final java.nio.file.Path pipePath = java.nio.file.FileSystems.getDefault().getPath("\\\\.\\pipe\\beatTheLucy");
+            logger.info( String.valueOf( pipePath ) );
+            try(
+                final java.nio.channels.FileChannel fileChannel =
+                java.nio.channels.FileChannel.open( pipePath ,
+                                                    java.nio.file.StandardOpenOption.CREATE_NEW,
+                                                    java.nio.file.StandardOpenOption.READ );
+                ){
+                
+            }catch( final IOException ioe ){
+                logger.severe( String.valueOf( ioe ));
+                /*
+                  1月 03, 2023 4:22:33 午前 com.example.Win32 main
+                  重大: java.nio.file.NoSuchFileException: \\.\pipe\beatTheLucy
+                  
+                  WinAPI の NamedPipe は CreateNamedPipe() で作る必要があるので、FileChannel では作成出来ない。
+                */
+            }
         }
-        
+
+        return;
     }
 }
